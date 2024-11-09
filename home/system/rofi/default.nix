8a7b6d1fd4eacd13866160730a4101a06e410637
Mauricio Barg <mrwbarg@gmail.com>
2024-11-09 19:22:05 -0300

    rofi

--- home/system/rofi/default.nix
+++ home/system/rofi/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, home-manager, ... }:
+{ pkgs, home-manager, ... }:
 {
   imports = [
     home-manager.nixosModules.home-manager
@@ -15,17 +15,112 @@
       rofi = {
         enable = true;
         package = pkgs.rofi-wayland;
-        extraConfig =
+        extraConfig = {
+          modi = "drun,calc";
+          sidebar-mode = true;
+          display-drun = "   Apps ";
+          display-calc = " Calculator";
+        };
+        theme =
           let
             inherit (config.lib.formats.rasi) mkLiteral;
           in
-          {
-            modi = "drun,calc";
-            sidebar-mode = true;
-            display-drun = "   Apps ";
-            display-calc = " Calculator";
+          lib.mkForce {
+            "*" = {
+                bg-col = mkLiteral "#${config.lib.stylix.colors.base00}";
+                bg-col-light = mkLiteral "#${config.lib.stylix.colors.base00}";
+                border-col = mkLiteral "#${config.lib.stylix.colors.base00}";
+                selected-col = mkLiteral "#${config.lib.stylix.colors.base00}";
+                blue = mkLiteral "#${config.lib.stylix.colors.base0D}";
+                fg-col = mkLiteral "#${config.lib.stylix.colors.base05}";
+                fg-col2 = mkLiteral "#${config.lib.stylix.colors.base08}";
+                grey = mkLiteral "#${config.lib.stylix.colors.base04}";
+                width = 800;
+                font = "FiraCode Nerd Font 12";
+            };
+            "window" = {
+                height = mkLiteral "360px";
+                border = mkLiteral "3px";
+                border-color = mkLiteral "@border-col";
+                background-color = mkLiteral "@bg-col";
+            };
+            "element-text, element-icon, mode-switcher" = {
+                background-color = mkLiteral "inherit";
+                text-color = mkLiteral "inherit";
+            };
+            "mainbox" = {
+                background-color = mkLiteral "@bg-col";
+            };
+            "inputbar" = {
+                children = map mkLiteral [ "prompt" "entry" ];
+                background-color = mkLiteral "@bg-col";
+                border-radius = mkLiteral "5px";
+                padding = mkLiteral "2px";
+            };
+            "prompt" = {
+                background-color = mkLiteral "@blue";
+                padding = mkLiteral "6px";
+                text-color = mkLiteral "@bg-col";
+                border-radius = mkLiteral "3px";
+                margin = mkLiteral "20px 0px 0px 20px";
+            };
+            "textbox-prompt-colon" = {
+                expand = mkLiteral "false";
+                str = ":";
+            };
+            "entry" = {
+                padding = mkLiteral "6px";
+                margin = mkLiteral "20px 0px 0px 10px";
+                text-color = mkLiteral "@fg-col";
+                background-color = mkLiteral "@bg-col";
+            };
+            "listview" = {
+                border = mkLiteral "0px 0px 0px";
+                padding = mkLiteral "6px 0px 0px";
+                margin = mkLiteral "10px 0px 0px 20px";
+                columns = 1;
+                lines = 5;
+                background-color = mkLiteral "@bg-col";
+            };
+            "element" = {
+              padding = mkLiteral "5px";
+              background-color = mkLiteral "@bg-col";
+              text-color = mkLiteral "@fg-col";
+            };
+            "element-icon" = {
+              size = mkLiteral "25px";
+            };
+            "element selected" = {
+              background-color = mkLiteral "@selected-col";
+              text-color = mkLiteral "@fg-col2";
+            };
+            "mode-switcher" = {
+              spacing = 0;
+            };
+            "button" = {
+              padding = mkLiteral "10px";
+              background-color = mkLiteral "@bg-col-light";
+              text-color = mkLiteral "@grey";
+              vertical-align = mkLiteral "0.5";
+              horizontal-align = mkLiteral "0.5";
+            };
+            "button selected" = {
+              background-color = mkLiteral "@bg-col";
+              text-color = mkLiteral "@blue";
+            };
+            "message" = {
+              background-color = mkLiteral "@bg-col-light";
+              margin = mkLiteral "2px";
+              padding = mkLiteral "2px";
+              border-radius = mkLiteral "5px";
+            };
+            "textbox" = {
+              padding = "6px";
+              margin = "20px 0px 0px 20px";
+              text-color = "@blue";
+              background-color = "@bg-col-light";
+            };
           };
-        theme = lib.mkForce "~/.local/share/rofi/themes/catppuccin-mocha.rasi";
         plugins = [
           (pkgs.rofi-calc.override {
             rofi-unwrapped = pkgs.rofi-wayland-unwrapped;
